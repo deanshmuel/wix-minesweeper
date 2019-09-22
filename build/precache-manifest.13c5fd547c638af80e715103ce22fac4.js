87e7ba746024ce5992ce3f2252d0a63c577ab1a6
deanshmuel <dshmuel110@gmail.com>
2019-09-22 14:18:17 +0300

    with dist folder

---
+++ build/precache-manifest.13c5fd547c638af80e715103ce22fac4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c85cf9d97fd9a723aa1d8c1da34c05fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8489c8555380f1906b2b",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "a67c2c30452f776b07e1",
+    "url": "/static/css/main.8a84f0f9.chunk.css"
+  },
+  {
+    "revision": "8489c8555380f1906b2b",
+    "url": "/static/js/2.e9e311a0.chunk.js"
+  },
+  {
+    "revision": "a67c2c30452f776b07e1",
+    "url": "/static/js/main.5d6f96cb.chunk.js"
+  },
+  {
+    "revision": "89fb2a38088f88340598",
+    "url": "/static/js/runtime-main.380b77f4.js"
+  }
+]);
